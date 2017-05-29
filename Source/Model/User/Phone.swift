61dd927307d334e23316e4774959a16db5239a66
Sandro Machado <sandro@uphold.com>
2017-05-29 10:49:29 +0100

    Remove phone's `nationalMasked` and `internationalMasked`

--- Source/Model/User/Phone.swift
+++ Source/Model/User/Phone.swift
@@ -10,12 +10,6 @@ public class Phone: Mappable {
     /// The E.164 phone mask.
     public private(set) final var e164Masked: String?
 
-    /// The international phone mask.
-    public private(set) final var internationalMasked: String?
-
-    /// The national phone mask.
-    public private(set) final var nationalMasked: String?
-
     /// A boolean indicating if the phone is the primary user phone.
     public private(set) final var primary: Bool?
 
@@ -27,16 +21,12 @@ public class Phone: Mappable {
 
       - parameter id: The contact id.
       - parameter e164Masked: The E.164 phone mask.
-      - parameter internationalMasked: The international phone mask.
-      - parameter nationalMasked: The national phone mask.
       - parameter primary: A boolean indicating if the phone is the primary user phone.
       - parameter verified: A boolean indicating if the phone is verified.
     */
-    public init(id: String, e164Masked: String, internationalMasked: String, nationalMasked: String, primary: Bool, verified: Bool) {
+    public init(id: String, e164Masked: String, primary: Bool, verified: Bool) {
         self.id = id
         self.e164Masked = e164Masked
-        self.internationalMasked = internationalMasked
-        self.nationalMasked = nationalMasked
         self.primary = primary
         self.verified = verified
     }
@@ -59,8 +49,6 @@ public class Phone: Mappable {
     public func mapping(map: Map) {
         self.id <- map["id"]
         self.e164Masked <- map["e164Masked"]
-        self.internationalMasked <- map["internationalMasked"]
-        self.nationalMasked <- map["nationalMasked"]
         self.primary <- map["primary"]
         self.verified <- map["verified"]
     }
