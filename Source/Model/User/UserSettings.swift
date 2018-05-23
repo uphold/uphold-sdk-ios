aefd8e4e84f03da17e6dcd449f31cce9e731a3e2
Sandro Machado <sandro@uphold.com>
2018-05-23 14:59:15 +0100

    Add `hasMarketingConsent` setting

--- Source/Model/User/UserSettings.swift
+++ Source/Model/User/UserSettings.swift
@@ -7,6 +7,9 @@ open class UserSettings: Mappable {
     /// The user's default currency.
     open private(set) var currency: String?
 
+    /// A boolean indicating if the user has the marketing consent enabled.
+    open private(set) var hasMarketingConsent: Bool?
+
     /// A boolean indicating if the user has the news subscription enabled.
     open private(set) var hasNewsSubscription: Bool?
 
@@ -23,12 +26,13 @@ open class UserSettings: Mappable {
       Constructor.
 
       - parameter currency: The user's default currency.
+      - parameter hasMarketingConsent: A boolean indicating if the user has the marketing consent enabled.
       - parameter hasNewsSubscription: A boolean indicating if the user has the news subscription enabled.
       - parameter intl: The user internationalization settings.
       - parameter otp: The user otp settings.
       - parameter theme: The user theme.
     */
-    public init(currency: String, hasNewsSubscription: Bool, intl: InternationalizationUserSettings, otp: Otp, theme: String) {
+    public init(currency: String, hasMarketingConsent: Bool, hasNewsSubscription: Bool, intl: InternationalizationUserSettings, otp: Otp, theme: String) {
         self.currency = currency
         self.hasNewsSubscription = hasNewsSubscription
         self.intl = intl
@@ -53,6 +57,7 @@ open class UserSettings: Mappable {
     */
     open func mapping(map: Map) {
         currency  <- map["currency"]
+        hasMarketingConsent  <- map["hasMarketingConsent"]
         hasNewsSubscription  <- map["hasNewsSubscription"]
         intl  <- map["intl"]
         otp  <- map["otp"]
